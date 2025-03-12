deca7c9caa2e92b35ff4578fea88467252113319
mthipanema <42364575+mthipanema@users.noreply.github.com>
2025-03-12 00:15:28 -0300

    Update backend_calculadora.py
    Mudança no função de calculo

--- backend_calculadora.py
+++ backend_calculadora.py
@@ -1,29 +1,23 @@
+def soma(a: float, b: float) -> float:
+    """Realiza a soma de dois números"""
+    return a + b
 
-a = float(input("Informe A:  "))
-b = float(input("Informe B: "))
-menu = int(input("Digite menu 1-6"))
-match menu:
-    #Soma
-    case 1:
-        #print(a+b)
-        c=(a+b)
-    #Subtração
-    case 2:
-        #print(a-b)
-        c=(a-b)
-    #Multiplicação
-    case 3:
-        #print(a*b)
-        c=(a*b)
-    #Divisão
-    case 4:
-        #print(a/b)
-        c=(a/b)
-    #Potencia
-    case 5:
-        #print(a**b)
-        c=(a**b)
-    #Raiz
-    case 6:
-       #print(a**(1/b))
-        c=(a**(1/b))
+def subtracao(a: float, b: float) -> float:
+    """Realiza a subtração de dois números"""
+    return a - b
+
+def multiplicacao(a: float, b: float) -> float:
+    """Realiza a multiplicação de dois números"""
+    return a * b
+
+def divisao(a: float, b: float) -> float:
+    """Realiza a divisão de dois números"""
+    return a / b
+
+def potencia(a: float, b: float) -> float:
+    """Calcula a potência de a elevado a b"""
+    return a ** b
+
+def raiz(a: float, b: float) -> float:
+    """Calcula a raiz b-ésima de a"""
+    return a ** (1/b)
